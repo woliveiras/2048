c2df9996798f453e162b6ed11235a438954470af
William Oliveira <w.oliveira542@gmail.com>
2025-04-03 06:58:04 +0200

    feat: create the board

---
+++ Assets/Scripts/TileState.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TileState", menuName = "Tile State", order = 0)]
+public class TileState : ScriptableObject
+{
+  public Color backgroundColor;
+  public Color textColor;
+}
